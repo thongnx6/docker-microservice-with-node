e48af0d23c6aba2b396e8e75b1af58cba6a9e4c8
Thong Nguyen <thongnguyen@Thongs-MacBook-Pro-2.local>
2018-07-18 12:27:52 +0800

    Fix ER_NOT_SUPPORTED_AUTH_MODE by way alter change plugin to *mysql_native_password* instead of *caching_sha2_password*

---
+++ test-db/fix_support_authmode.sql
@@ -0,0 +1,3 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '123';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123';
+ALTER USER 'users_service'@'%' IDENTIFIED WITH mysql_native_password BY '123';
